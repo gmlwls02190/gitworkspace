8036b3dc16d5a2fc0101e99b885a00bddecfaa51
Hee JIN <Hee JIN@61.79.192.126>
2021-08-26 02:40:19 +0900

    CodeUp 기초예제1001~1023

---
+++ CodeUp/src/test1001_1008/test.java
@@ -0,0 +1,18 @@
+package test1001_1008;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("Hello"); // 1001
+		System.out.println("Hello World"); // 1002
+		System.out.println("Hello\nWorld"); // 1003
+		System.out.println("\'Hello\'"); // 1004
+		System.out.println("\"Hello World\""); // 1005
+		System.out.println("\"!@#$%^&*\""); // 1006
+		System.out.println("\"C:\\Download\\hello.java\""); // 1007
+		System.out.println("\u250C \u252C \u2510\n\u251C \u253C \u2524\n\u2514 \u2534 \u2518"); // 1008
+		
+		
+		
+		
+	}
+}
---
+++ CodeUp/src/test1010_1027/test.java
@@ -0,0 +1,103 @@
+package test1010_1027;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class test {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		// 1010
+		/*int n;
+		System.out.print("정수입력: ");
+		n=sc.nextInt();
+		System.out.println(n);*/ 
+		
+		// 1011
+		/*String str;
+		System.out.print("문자입력: ");
+		str=sc.next();
+		System.out.println(str);*/ 
+		
+		// 1012
+		/*double a;
+		System.out.print("실수입력: ");
+		a=sc.nextDouble();
+		System.out.println(a);*/ 
+		
+		// 1013
+		/*int a,b;
+		System.out.print("정수 2개입력: ");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(a+" "+b);*/ 
+		
+		// 1014
+		/*String str1,str2;
+		System.out.print("문자 2개입력: ");
+		str1=sc.next();
+		str2=sc.next();
+		System.out.println(str2+" "+str1);*/ 
+		
+		// 1015
+		/*double a;
+		System.out.print("실수 입력: ");
+		a=sc.nextDouble();
+		System.out.printf("%.2f", a);*/
+		
+		// 1017
+		/*int a;
+		System.out.print("정수 입력: ");
+		a=sc.nextInt();
+		System.out.println(a+" "+a+" "+a);*/
+		
+		// 1018
+		/*int h,m;
+		System.out.print("현재 시간 분 입력: ");
+		h=sc.nextInt();
+		m=sc.nextInt();
+		System.out.println(h+":"+m);*/
+		
+		// 1019
+		/*int y,m,d;
+		System.out.print("년 월 일 입력: ");
+		y=sc.nextInt();
+		m=sc.nextInt();
+		d=sc.nextInt();
+		System.out.printf("%04d.%02d.%02d", y,m,d);*/
+		
+		// 1020
+		/*String id;
+		System.out.printf("주민번호 입력: ");
+		id=sc.next();
+		String[] arr=id.split("\\-");
+		System.out.println(arr[0]+arr[1]);*/
+		
+		// 1021
+		/*String str;
+		System.out.print("단어 입력: ");
+		str=sc.next();
+		System.out.println(str);*/
+		
+		// 1022
+		/*String str;
+		System.out.print("문장 입력: ");
+		str=sc.nextLine();
+		System.out.println(str);*/
+		
+		// 1023
+		String str;
+		System.out.printf("실수 입력: ");
+		str=sc.next();
+		String[] arr=str.split("\\.");
+		System.out.println(arr[0]+"\n"+arr[1]);
+		
+		// 1024
+		
+		// 1025
+		
+		// 1026
+		
+		// 1027
+	}
+}

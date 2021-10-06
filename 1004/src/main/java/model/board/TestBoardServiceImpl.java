4f06d8ffb8ec3770c1dfec2c7a3b57cc15a000a7
gmlwls02190 <ksh02190@gmail.com>
2021-10-06 18:30:26 +0900

    스프링 AOP를 활용한 로깅처리 실습 예제

--- 1004/src/main/java/model/board/TestBoardServiceImpl.java
+++ 1004/src/main/java/model/board/TestBoardServiceImpl.java
@@ -14,37 +14,28 @@ public class TestBoardServiceImpl implements TestBoardService{
 	private TestBoardDAO tbDAO;
 	private LogPlusAdvice logPlusAdvice;
 	
-	public TestBoardServiceImpl() {
-		this.logPlusAdvice=new LogPlusAdvice();
-	}
-	
 	@Override
 	public void insertTBoard(TestBoardVO vo) {
-		logPlusAdvice.printPlusLog();
 		tbDAO.insertTBoard(vo);
 	}
 
 	@Override
 	public void updateTBoard(TestBoardVO vo) {
-		logPlusAdvice.printPlusLog();
 		tbDAO.updateTBoard(vo);
 	}
 
 	@Override
 	public void deleteTBoard(TestBoardVO vo) {
-		logPlusAdvice.printPlusLog();
 		tbDAO.deleteTBoard(vo);
 	}
 
 	@Override
 	public List<TestBoardVO> selectAll() {
-		logPlusAdvice.printPlusLog();
 		return tbDAO.selectAll();
 	}
 
 	@Override
 	public TestBoardVO selectOne(TestBoardVO vo) {
-		logPlusAdvice.printPlusLog();
 		return tbDAO.selectOne(vo);
 	}
 
